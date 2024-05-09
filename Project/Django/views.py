8bcb772286f1e90cc85ec12984e81d36ececa445
Timothy itayi <timmytam10@gmail.com>
2024-05-09 17:32:09 +1200

    add .env to .gitignore , add openApi url

---
+++ Project/Django/views.py
@@ -0,0 +1,14 @@
+from django.http import JsonResponse
+import requests
+
+def get_openf1_data(request):
+    # Make a GET request to the OpenF1 API
+    response = requests.get('https://api.openf1.org/v1/some-endpoint')
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Return the JSON data from the OpenF1 API
+        return JsonResponse(response.json())
+    else:
+        # Return an error message if the request failed
+        return JsonResponse({'error': 'Failed to fetch data from OpenF1 API'}, status=500)
--- python/api.py
+++ python/api.py
@@ -1,1 +1,6 @@
 ##create api code for back end ##
+ import requests
+ api_url = "https://api.openf1.org/v1/drivers"
+ response = requests.get(api_url)
+ response.json()
+{'driver_number': , 'full_name ': , 'team_name': }
